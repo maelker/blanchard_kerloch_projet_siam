a9ad809edd172fab0d200f6e7b0840981e4e033b
skydread1 <loic.blanchard01@gmail.com>
2015-12-07 15:56:39 +0100

    api_siam modif

--- src/api_siam.c
+++ src/api_siam.c
@@ -1,5 +1,11 @@
 #include "api_siam.h"
 #include "plateau_modification.h"
+#include "coordonnees_plateau.h"
+#include "jeu_siam.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 coup_jeu api_siam_tenter_introduire_nouvelle_piece_si_possible(jeu_siam* jeu,
                                                                int x,int y,
@@ -28,8 +34,22 @@ coup_jeu api_siam_tenter_deplacer_piece_si_possible(jeu_siam* jeu,
 
 coup_jeu api_siam_tenter_changer_orientation_piece_si_possible(jeu_siam* jeu,int x,int y,orientation_deplacement orientation)
 {
-    //coder cette fonction
-    coup_jeu coup;
-    coup_jeu_initialiser(&coup);
-    return coup;
+  
+  assert(jeu!=NULL);
+  assert(jeu_etre_integre(jeu)==1);
+  coup_jeu coup;
+  coup_jeu_initialiser(&coup);
+  
+  if(jeu_verifier_type_piece_a_modifier(jeu,x,y)==1)
+  {
+    if(coordonnees_etre_dans_plateau(x,y)==1)
+    {
+      if (plateau_modification_changer_orientation_piece_etre_possible(&(jeu->plateau),x,y,orientation)==1)
+      {
+	coup.valide=1;
+      }
+      
+    }
+  }
+  return coup;
 }
