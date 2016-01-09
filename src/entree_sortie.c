5fdd73b844e11c447ef6059e488a42f298b88133
Mael Kerloch <mael.kerloch@gmail.com>
2016-01-09 15:32:01 +0100

    entree_sortie normalement finigit add entree_sortie.c git add entree_sortie.c  :)

--- src/entree_sortie.c
+++ src/entree_sortie.c
@@ -80,67 +80,50 @@ void entree_sortie_ecrire_jeu_fichier(const char* filename,const jeu_siam* jeu)
 
 void entree_sortie_lire_jeu_fichier(const char* filename,jeu_siam* jeu)
 {
-    jeu_siam _;
-    int *_00x0=&_.joueur;
+    FILE* fid=fopen(filename,"r");
     
-    size_t (*_0x50)(const char*)=strlen;
-    piece_siam* _00x02=*_.plateau.piece,*_0x02,_0x03;
-    
-    int _00x00_,_10x01_=0x00;
-    char _0x100[0x100],_01x10_[0x05][0x100];
-    
-    const char* _for="Erreur ouverture fichier %s\n",*_jeu="Erreur lecture numero de ligne",*_while="Jeu invalide lecture fichier %s\n";
-    
-    jeu_initialiser(&_);
-    
-    FILE *_0x100_=fopen(filename,"r");
-    
-    assert(_0x100_!=0x00);
-    if(_0x100_==0x00)
+    assert(fid!=NULL);
+    if(fid==NULL)
     {
-        printf(_for,filename);
+        printf("Erreur ouverture fichier %s\n",filename);
         exit(1);
-        
     }
-    while(fgets(_0x100,0x100,_0x100_)!=0)
+
+    jeu_siam nouveau_jeu;
+    jeu_initialiser(&nouveau_jeu);
+    int  *nouveau_joueur=&nouveau_jeu.joueur;
+    
+    char ligne_texte[100];    
+    while(fgets(ligne_texte,100,fid)!=0)
     {
-        _00x00_=0x00;
-        if(_0x50(_0x100)>=011&&strncmp(_0x100,"joueur 0 (elephant)",0x13)==0x00)
-        {
-            *_00x0=0x00;
-        }
+        int ligne=0;
+        if(strncmp(ligne_texte,"joueur 0 (elephant)",8)==0)
+            *nouveau_joueur=0;
         else 
-            if(_0x50(_0x100)>=011&&strncmp(_0x100,"joueur 1 (rhinoceros)",0x13)==0x00)
-            {
-                *_00x0=0x01;
-                
-            }
+            if(strncmp(ligne_texte,"joueur 1 (rhinoceros)",8)==0)
+                *nouveau_joueur=1;
             else 
-                if(sscanf(_0x100,"[%d] %s | %s | %s | %s | %s |",&_00x00_,
-                    0x00[_01x10_],0x01 [_01x10_],0x02[_01x10_],0x03[_01x10_],0x04[_01x10_])==0x06&&0x08>0x02)
+            {
+                char piece_presente[5][100];
+                if(sscanf(ligne_texte,"[%d] %s | %s | %s | %s | %s |",&ligne,piece_presente[0],piece_presente[1],piece_presente[2],piece_presente[3],piece_presente[4])==6)
                 {
-                    if(_00x00_<0x00||_00x00_>0x04)
+                    if(ligne<0||ligne>4)
                     {
-                        puts(_jeu);
+                        puts("Erreur lecture numero de ligne");
                         abort();
-                        
                     }
-                    for(_10x01_=0x00;
-                        _10x01_<0x05;_10x01_+=0x01)
-                        {
-                            _0x02=&(0x05*_10x01_+_00x00_)[_00x02];
-                            _0x03=piece_correspondre_nom_cours(_10x01_[_01x10_]);
-                            *_0x02=_0x03;
-                            
-                        }
-                        
+                    int colonne=0;
+                    piece_siam *piece_intermediaire;
+                    for(colonne=0;colonne<5;++colonne)
+                    {
+                        piece_intermediaire=plateau_obtenir_piece(&jeu->plateau,colonne,ligne);
+                        *piece_intermediaire=piece_correspondre_nom_cours(colonne[piece_presente]);
+                    }
                 }
-                
+            }
     }
-    
-    if(jeu_etre_integre(&_))*jeu=_;
-    else printf(_while,filename);
-    
+    if(jeu_etre_integre(&nouveau_jeu)==0);
+    else printf("Jeu invalide lecture fichier %s\n",filename);
 }
 
 
@@ -166,34 +149,33 @@ void entree_sortie_ecrire_jeu_pointeur_fichier(FILE* identifiant,const jeu_siam*
 void entree_sortie_ecrire_plateau_pointeur_fichier(FILE* identifiant,const plateau_siam* plateau)
 {
     FILE* fid=identifiant;
+    
     if (fid==NULL)
     {
         printf("Erreur ouverture du fichier\n");
         exit(1);
     }
     
-    const piece_siam* piece_plateau, *piece_presente=*plateau->piece;
+    const piece_siam *piece_presente=*plateau->piece;
     int colonne=0;
     int ligne=0;
-    for(ligne=4;ligne>=0;--ligne)
+    for(ligne=NBR_CASES-1;ligne>=0;--ligne)
     {
         fprintf(fid,"[%d] ",ligne);
         colonne=0;
-        for(colonne=0;colonne<010-03 && 05<0x080;++colonne)
+        for(colonne=0;colonne<NBR_CASES;++colonne)
         {
-            piece_plateau=&(5*colonne+ligne)[piece_presente];
-            //piece_plateau=plateau_obtenir_piece(plateau,colonne,ligne);
-            
-            fprintf(fid,"%s",type_nommer_nom_cours(piece_plateau->type));
-            if(piece_plateau->type==elephant || piece_plateau->type==rhinoceros)
-                fprintf(fid,"-%s",orientation_nommer_nom_cours(piece_plateau->orientation));
-            
+            piece_presente=plateau_obtenir_piece_info(plateau,colonne,ligne);
+            fprintf(fid,"%s",type_nommer_nom_cours(piece_presente->type));
+            if(piece_presente->type==elephant || piece_presente->type==rhinoceros)
+                fprintf(fid,"-%s",orientation_nommer_nom_cours(piece_presente->orientation));
             fprintf(fid," | ");
         }
         fprintf(fid,"\n");
     }
     
     fprintf(fid,"    ");
+    
     colonne=0;
     for(colonne=0;colonne<5;++colonne)
     {
@@ -202,6 +184,3 @@ void entree_sortie_ecrire_plateau_pointeur_fichier(FILE* identifiant,const plate
     fprintf(fid,"\n");
     
 }
-
-
-
